787add970d51b2c0f425f3b44488b02e07a9774f
Любомир Капов <kapoliub@Lubomirs-MacBook-Pro.local>
2019-11-27 23:32:03 +0200

    rewrited main.js with jquery & added key events

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,16 +1,16 @@
-//_____open/close new employee form_________________
 function addNewEmployeeButtonClick(){
     $('#employeeForm').toggleClass('hidden');
-    focusMethod();
+    getFocus();
     if($('#addTitle').hasClass('hidden')){
         closeForm();
         $('#employeeForm').toggleClass('hidden');
     }
 }
-focusMethod = function getFocus() {           
+
+function getFocus() {           
     $("#nameInput").focus();
 }
-//_____main checkbox changer_______________
+
 function changeCheckboxesValue(elem){
     let checkBoxes = $('.checkboxData');
     if(elem.checked){
@@ -30,7 +30,7 @@ function changeCheckboxesValue(elem){
         // })
     }
 }
-//_____________delete checked items____________________
+
 function deleteSelectedItems(){
     let checkboxes = $('.checkboxData');
     let tr = $('.userRow');
@@ -39,10 +39,10 @@ function deleteSelectedItems(){
             tr[i].remove();
         }
     }
-    $('#mainCheckbox').checked = false;
+    document.getElementById('mainCheckbox').checked = false;
     closeForm();
 }
-//_________reset form to default values____________
+
 function closeForm(){
     $('#addTitle').removeClass('hidden');
     $('#editTitle').addClass('hidden');
@@ -55,12 +55,12 @@ function closeForm(){
     $('#addressInput').val('');
     $('#phoneInput').val('');
 }
-//__________delete row __________________________
+
 function deleteUserRow(id){
     $('#' + id).remove();
     closeForm();
 }
-//___________edit button click___________________
+
 function editButtonClick(id){
     let oldUserName = $('#' + id + ' .userName');
     let oldUserEmail = $('#' + id + ' .userEmail');
@@ -77,11 +77,11 @@ function editButtonClick(id){
     $('#addEmployee').addClass('hidden');
     $('#hidenEditBtn').removeClass('hidden')
     $('#employeeForm').removeClass('hidden');
-    focusMethod();
+    getFocus();
     //______save our ID________
     $('#employeeForm').attr('customAttrID', id);
 }
-//__________update employee data_________________
+
 function updateEmployeeInfo(){
     let newUserData = {
         name : $('#nameInput').val(),
@@ -100,89 +100,78 @@ function updateEmployeeInfo(){
     $('#employeeForm').removeAttr('customAttrID');
     closeForm();
 }
-//_________create a new element__________________
+
 function createNewElement(){
     let randomID = String('userID-' + Math.random() * 1000000).split('.')[0];
-    let employeeForm = document.querySelector('#employeeForm');
-    let userName = document.querySelector('#employeeForm #nameInput');
-    let userEmail = document.querySelector('#employeeForm #emailInput');
-    let userAddress = document.querySelector('#employeeForm #addressInput');
-    let userPhone = document.querySelector('#employeeForm #phoneInput');
-    if(userName.value.trim().length < 1 || userEmail.value.trim().length < 1 || userAddress.value.trim().length < 1 || userPhone.value.trim().length < 1){
-        return;
+    if($('#nameInput').val().trim().length > 1){
+        if($('#emailInput').val().trim().length > 1 && $('#emailInput').val().indexOf('@') >= 0){
+            if($('#addressInput').val().trim().length > 1){
+                if($('#phoneInput').val().trim().length > 9){
+                    $('<tr>',{
+                        id : randomID,
+                        class : 'userRow'
+                    }).appendTo($('#inputDataArea'));
+                    $('<td>',{
+                        class : 'userCheckbox'
+                    }).appendTo($('#' + randomID));
+                    $('<td>',{
+                        class : 'userName'
+                    }).appendTo($('#' + randomID));
+                    $('<td>',{
+                        class : 'userEmail'
+                    }).appendTo($('#' + randomID));
+                    $('<td>',{
+                        class : 'userAddress'
+                    }).appendTo($('#' + randomID));
+                    $('<td>',{
+                        class : 'userPhone'
+                    }).appendTo($('#' + randomID));
+                    $('<td>',{
+                        class : 'userButtonsArea'
+                    }).appendTo($('#' + randomID));
+                
+                    $('<input>', {
+                        type : 'checkbox',
+                        class : 'checkboxData'
+                    }).appendTo($('#' + randomID + ' .userCheckbox'));
+                    
+                    $('<button>',{
+                        class : 'editBtn',
+                        'data-title' : 'Edit'
+                    }).appendTo($('#' + randomID + ' .userButtonsArea'))
+                    $('<button>',{
+                        class : 'deleteBtn',
+                        'data-title' : 'Delete'
+                    }).appendTo($('#' + randomID + ' .userButtonsArea'))
+                
+                    $('<i>',{
+                        class : 'fas fa-pen'
+                    }).appendTo($('#' + randomID + ' .editBtn'))
+                    $('<i>',{
+                        class : 'fas fa-trash-alt'
+                    }).appendTo($('#' + randomID + ' .deleteBtn'))
+                
+                    $('#' + randomID + ' .editBtn').on('click', function(){
+                        editButtonClick(randomID);
+                    })
+                    
+                    $('#' + randomID + ' .deleteBtn').on('click', function(){
+                        deleteUserRow(randomID);
+                    })
+                
+                    $('#' + randomID + ' .userName').text($('#nameInput').val());
+                    $('#' + randomID + ' .userEmail').text($('#emailInput').val());
+                    $('#' + randomID + ' .userAddress').text($('#addressInput').val());
+                    $('#' + randomID + ' .userPhone').text($('#phoneInput').val());
+                
+                    closeForm();
+                }
+            }
+        }
     }
-    //__________________________________________
-    let userDataTable = document.querySelector('#userDataTable');
-    let userRow = document.createElement('tr');
-    //__________________________________________
-    let userRowInput = document.createElement('td');
-    let userRowName = document.createElement('td');
-    let userRowEmail = document.createElement('td');
-    let userRowAddress = document.createElement('td');
-    let userRowPhone = document.createElement('td');
-    let userRowButtonsArea = document.createElement('td');
-    //_______________________________________________
-    let userRowCheckbox = document.createElement('input');
-    let editButton = document.createElement('button');
-    let deleteButton = document.createElement('button');
-    //_____________________________________
-    userRowInput.appendChild(userRowCheckbox);
-    userRowCheckbox.setAttribute('type', 'checkbox');
-    userRowCheckbox.classList.add('checkboxData');
-
-    editButton.classList.add('editBtn');
-    editButton.setAttribute('data-title', 'Edit');
-    let penIcon = document.createElement('i');
-    penIcon.classList.add('fas');
-    penIcon.classList.add('fa-pen');
-    editButton.append(penIcon);
-    editButton.addEventListener('click', function(){
-        editButtonClick(randomID);
-    })
-
-    deleteButton.classList.add('deleteBtn');
-    deleteButton.setAttribute('data-title', 'Delete');
-    let trashIcon = document.createElement('i');
-    trashIcon.classList.add('fas');
-    trashIcon.classList.add('fa-trash-alt');
-    deleteButton.append(trashIcon);
-    deleteButton.addEventListener('click', function(){
-        deleteUserRow(randomID);
-    })
-
-    userRowButtonsArea.appendChild(editButton);
-    userRowButtonsArea.appendChild(deleteButton);
-    //_____________________________________________
-    userRow.appendChild(userRowInput);
-    userRow.appendChild(userRowName);
-    userRow.appendChild(userRowEmail);
-    userRow.appendChild(userRowAddress);
-    userRow.appendChild(userRowPhone);
-    userRow.appendChild(userRowButtonsArea);
-
-    userDataTable.appendChild(userRow);
-
-    userRow.setAttribute('id', randomID);
-
-    userRow.classList.add('userRow')
-    userRowName.classList.add('userName');
-    userRowEmail.classList.add('userEmail');
-    userRowAddress.classList.add('userAddress');
-    userRowPhone.classList.add('userPhone');
-
-    userRowName.innerText = userName.value;
-    userRowEmail.innerText = userEmail.value;
-    userRowAddress.innerText = userAddress.value;
-    userRowPhone.innerText = userPhone.value;
-    //_________________________________________
-    userName.value = '';
-    userEmail.value = '';
-    userAddress.value = '';
-    userPhone.value = '';
-    closeForm();
 }
 
-// _________key event_________
+// _________key events_________
 $(window).on('keyup', function(e){
     if(e.keyCode == 27){
         closeForm();
@@ -194,6 +183,21 @@ $(window).on('keyup', function(e){
     }
 });
 
+$('#nameInput').on('keyup', function(e){
+    if(e.keyCode == 13){
+        $('#addEmployee').click();
+    }
+})
+$('#emailInput').on('keyup', function(e){
+    if(e.keyCode == 13){
+        $('#addEmployee').click();
+    }
+})
+$('#addressInput').on('keyup', function(e){
+    if(e.keyCode == 13){
+        $('#addEmployee').click();
+    }
+})
 $('#phoneInput').on('keyup', function(e){
     if(e.keyCode == 13){
         $('#addEmployee').click();
